21c7188fb55a63d1f8bb402e0ddee70a177683f0
ShivamYadav <cse.18bcs2337@gmail.com>
2020-06-09 03:47:49 +0530

    minor typos fixed to merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { CartModule } from './cart/cart.module';
-=======
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
->>>>>>> da0d136fc893f534ecec2eb743ae41453284c493
 
 @NgModule({
   declarations: [
@@ -30,10 +27,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
     AppRoutingModule,
     HomeModule,
     ProductListModule,
-<<<<<<< HEAD
     CartModule
-=======
->>>>>>> da0d136fc893f534ecec2eb743ae41453284c493
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cart/cart-list/cart-list.component.ts
+++ src/app/cart/cart-list/cart-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductList } from 'src/app/product-list/product-list.modal';
+import { ProductList } from 'src/app/product-list/product-list.model';
 
 @Component({
   selector: 'app-cart-list',
