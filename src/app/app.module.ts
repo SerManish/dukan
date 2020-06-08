e768276e37ce26cc490d5e58348086ae863a94f3
ShivamYadav <cse.18bcs2337@gmail.com>
2020-06-09 04:14:48 +0530

    Signed-off-by: ShivamYadav <cse.18bcs2337@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { HomeModule} from './home/home.module';
 import { ProductListModule } from './product-list/product-list.module';
@@ -13,8 +12,6 @@ import { AppComponent } from './app.component';
 import { CartModule } from './cart/cart.module';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
-import { CartModule } from './cart/cart.module';
 
 
 @NgModule({
