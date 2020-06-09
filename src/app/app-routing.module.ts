59aa4bb62877946803e664de2c972b2b9d866a17
SerManish <cse.18bcs2830@gmail.com>
2020-06-09 15:51:12 +0530

    product-page module created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductPageComponent } from './product-page/product-page.component';
 
 const routes:Routes = [
     {path:'',redirectTo:'/home',pathMatch:'full'},
-    {path:'product', component: ProductPageComponent}
 ]
 
 @NgModule({
