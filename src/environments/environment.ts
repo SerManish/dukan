6f11682bfb2f2ed711b00e12deb67e314468d563
ShivamYadav <cse.18bcs2337@gmail.com>
2020-06-11 06:42:08 +0530

    Installation of angular fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDaBnvn0-M8dD-TnjD9pDd7W5wnwV58Fbw",
+    authDomain: "app-dukan.firebaseapp.com",
+    databaseURL: "https://app-dukan.firebaseio.com",
+    projectId: "app-dukan",
+    storageBucket: "app-dukan.appspot.com",
+    messagingSenderId: "768730303998",
+    appId: "1:768730303998:web:2f41cdd9aac60e294204af",
+    measurementId: "G-6H5F49M84T"
+  }
 };
 
 /*
