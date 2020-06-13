9e0a790582d0fa320ffb319a35cbdc5f1a56b545
Manish Kumar Giri <55207839+SerManish@users.noreply.github.com>
2020-06-13 23:47:13 +0530

    edit home component designed (#23)

--- src/app/admin/edit-home/edit-home.component.ts
+++ src/app/admin/edit-home/edit-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-home',
@@ -7,9 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditHomeComponent implements OnInit {
 
+  mode:string;
+  sliderForm:FormGroup;
+  imageArray:FormArray = new FormArray([]);
+  categoryForm:FormGroup;
+  categoryArray:FormArray = new FormArray([]);
+
   constructor() { }
 
   ngOnInit(): void {
+    this.sliderForm = new FormGroup({
+      'images': this.imageArray
+    });
+    this.categoryForm = new FormGroup({
+      'categories':this.categoryArray
+    });
+  }
+
+  changeHome()
+  {
+    console.table(this.sliderForm.value);
+  }
+
+  changeCategory()
+  {
+    console.table(this.categoryForm.value);
+  }
+
+  addNewImage()
+  {
+    (<FormArray>this.sliderForm.get('images')).push(
+      new FormGroup({
+        'source':new FormControl(null,Validators.required)
+      })
+    );
+  }
+
+  removeImage(index:number)
+  {
+    this.imageArray.controls.splice(index,1);
+    this.sliderForm.value['images'].splice(index,1);
+  }
+
+  addNewCategory()
+  {
+    (<FormArray>this.categoryForm.get('categories')).push(
+      new FormGroup({
+        'name':new FormControl(null,Validators.required),
+        'source':new FormControl(null,Validators.required)
+      })
+    );
+  }
+
+  removeCategory(index:number)
+  {
+    this.categoryArray.controls.splice(index,1);
+    this.categoryForm.value['categories'].splice(index,1);
+  }
+
+  clearForm()
+  {
+    this.sliderForm.reset();
   }
 
 }
