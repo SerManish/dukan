44b2aee51679203dadf3c7d41ca1413ef0c2937d
SerManish <cse.18bcs2830@gmail.com>
2020-06-15 03:28:22 +0530

    orders saved and fetched

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../shared/admin.service';
+import { AuthService } from '../shared/auth.service';
+import { Subscription } from 'rxjs';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public adminService:AdminService,
+    private authservice:AuthService
+  ) { }
+
+  orders = [];
+  products = new Map<string , object>();
+  
+  userSubscription:Subscription;
 
   ngOnInit(): void {
+    this.userSubscription = this.authservice.user.subscribe(
+      async (user) => {
+        if(user)
+        {
+          await this.adminService.recieveOrders(user.uid);
+          this.orders = this.adminService.orders;
+        }
+      }
+  );
+  console.log('1',this.adminService.orders);
+  
   }
-
 }
