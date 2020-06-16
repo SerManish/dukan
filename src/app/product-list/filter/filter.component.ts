d58f68aed6ee1506c1936cf075a10ab9a8da06c3
ShivamYadav <cse.18bcs2337@gmail.com>
2020-06-17 01:19:17 +0530

    search by category added

--- src/app/product-list/filter/filter.component.ts
+++ src/app/product-list/filter/filter.component.ts
@@ -1,58 +1,58 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AlertService } from 'src/app/shared/alert-bar/alert.service';
-import { BehaviorSubject } from 'rxjs';
 import { ProductService } from 'src/app/shared/product.service';
 
 @Component({
-  selector: 'app-filter',
-  templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.css']
+	selector: 'app-filter',
+	templateUrl: './filter.component.html',
+	styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
 
-  @ViewChild('filterForm') filterForm: ElementRef;
-  showFilter = false;
-  filter = {min: 0, max: 999999999, sortBy: 'Relevance'};
+	@ViewChild('filterForm') filterForm: ElementRef;
+	showFilter = false;
+	filter = { min: 0, max: 999999999, sortBy: 'Relevance' };
 
-  constructor(
-    private alertService: AlertService,
-    private productService: ProductService
-  ) {
-    this.productService.applyFilter.next(this.filter);
-  }
+	constructor(
+		private alertService: AlertService,
+		private productService: ProductService
+	) {
+		this.productService.applyFilter.next(this.filter);
+	}
 
-  toggleFilter()
-  {
-    this.showFilter=!this.showFilter;
-  }
+	toggleFilter() {
+		this.showFilter = !this.showFilter;
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  onApplyFilter(){
-    const form = this.filterForm.nativeElement;
-    this.filter.min = form.minimum.value==''? 0 : Number(form.minimum.value);
-    this.filter.max = form.maximum.value==''? 999999999 : Number(form.maximum.value);
-    this.filter.sortBy = form.sortBy.value;
+	//first priced are checked to in appropriate and the
+	//sends filter prefenreces to product list
+	onApplyFilter() {
+		const form = this.filterForm.nativeElement;
+		this.filter.min = form.minimum.value == '' ? 0 : Number(form.minimum.value);
+		this.filter.max = form.maximum.value == '' ? 999999999 : Number(form.maximum.value);
+		this.filter.sortBy = form.sortBy.value;
 
-    // console.log(this.filter);
-    if(this.filter.min>999999999 || this.filter.max>999999999){
-      this.alertService.alert('Maximum price should be less than 1000000000.');
-      return;
-    }
-    if(this.filter.min>this.filter.max){
-      this.alertService.alert('Minimum price should be less than Maximum price.');
-      return;
-    }
-    
-    this.productService.applyFilter.next(this.filter);
-  }
+		if (this.filter.min > 999999999 || this.filter.max > 999999999) {
+			this.alertService.alert('Maximum price should be less than 1000000000.');
+			return;
+		}
+		if (this.filter.min > this.filter.max) {
+			this.alertService.alert('Minimum price should be less than Maximum price.');
+			return;
+		}
 
-  onClear(){
-    const form = this.filterForm.nativeElement;
-    form.minimum.value = '';
-    form.maximum.value = '';
-    this.onApplyFilter();
-  }
+		this.productService.applyFilter.next(this.filter);
+	}
+
+	//clears the filter preferences
+	onClear() {
+		const form = this.filterForm.nativeElement;
+		form.minimum.value = '';
+		form.maximum.value = '';
+		this.onApplyFilter();
+	}
 
 }
