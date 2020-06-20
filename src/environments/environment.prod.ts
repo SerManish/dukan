1af1532d4ac316c3a3f95512c0dcc5ff407bdb7f
SerManish <cse.18bcs2830@gmail.com>
2020-06-21 00:41:15 +0530

    deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDaBnvn0-M8dD-TnjD9pDd7W5wnwV58Fbw",
+    authDomain: "app-dukan.firebaseapp.com",
+    databaseURL: "https://app-dukan.firebaseio.com",
+    projectId: "app-dukan",
+    storageBucket: "app-dukan.appspot.com",
+    messagingSenderId: "768730303998",
+    appId: "1:768730303998:web:2f41cdd9aac60e294204af",
+    measurementId: "G-6H5F49M84T"
+  }
 };
